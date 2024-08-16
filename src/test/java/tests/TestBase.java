e0be2d3c8839fec3e69b464a4eb1190d4a04c3e8
Evgeniy Mikhailov <jekkka23@mail.ru>
2024-08-16 10:39:12 +0500

    added banner

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,9 +22,9 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager"; //стратегия загрузки, если страница долго грузится, с ее помощью мы не будем дожидаться полной загрузки страницы
         Configuration.baseUrl = "https://citilink.ru"; //выносим основной сайт, а в тестах уже оставляем только конкретный путь к сайту
-        Configuration.holdBrowserOpen = true; //делает браузер закрытым после теста (true оставляет открытым)
+        //Configuration.holdBrowserOpen = true; //делает браузер закрытым после теста (true оставляет открытым)
         //Configuration.timeout = 5000; по дефолту таймаут 4000
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; //для удаленного запуска
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; //для удаленного запуска
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
